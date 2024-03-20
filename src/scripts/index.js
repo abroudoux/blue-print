dc94232c4e7ba5cb460e30fc2378955c0fb743c0
arthur <arthur.broudoux@gmail.com>
2024-03-20 10:57:54 +0100

    icon menu toggle

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,22 @@
 console.log('Js is working!')
 
 const iconMenuBurger = document.getElementById('icon-menu-burger');
+const iconOpenMenu = document.getElementById('icon-open-menu');
+const iconCloseMenu = document.getElementById('icon-close-menu');
+const menu = document.getElementById('menu');
+
+let isMenuOpen = false;
 
 iconMenuBurger.addEventListener('click', () => {
-  const menu = document.getElementById('menu');
-  menu.style.right = menu.style.right === '-20vw' ? '0' : '-20vw';
+  if (isMenuOpen) {
+    menu.style.right = '-25vw';
+    iconCloseMenu.style.display = 'none';
+    iconOpenMenu.style.display = 'block';
+  } else {
+    menu.style.right = '0';
+    iconCloseMenu.style.display = 'block';
+    iconOpenMenu.style.display = 'none';
+  }
+
+  isMenuOpen = !isMenuOpen;
 });
